fde755e4b0437196d9b70e5228fa8be896da2662
Steffen Eichenberg <Steffen.Eichenberg@lindenbaum.eu>
2017-04-14 16:29:40 +0200

    Add initial version of modularization strategy

---
+++ src/ModulesV2.elm
@@ -0,0 +1,42 @@
+module ModulesV2 exposing (..)
+
+import SumNodeV2 as SumNode
+import Html exposing (..)
+
+
+type alias Model =
+    SumNode.Model
+
+
+type alias Msg =
+    SumNode.Msg
+
+
+init : ( Model, Cmd Msg )
+init =
+    SumNode.init
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update =
+    SumNode.update
+
+
+view : Model -> Html Msg
+view =
+    SumNode.view
+
+
+subscriptions : Model -> Sub Msg
+subscriptions =
+    SumNode.subscriptions
+
+
+main : Program Never Model Msg
+main =
+    Html.program
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = subscriptions
+        }
